1544a5b0c5883b4d7333ab16a5fab206155ce5a0
Tanner <thobson125@gmail.com>
2012-12-31 00:06:34 -0500

    Added jar creation

---
+++ Engine/jar/create-windows.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function finish() {
+    rm org
+}
+
+trap finish exit
+
+root=..
+src=$root/src
+lib=$root/lib
+ln -s $lib org
+org=org
+lwjgl=$org/lwjgl-2.8.4/jar
+slick=$org/slick
+slick_util=$org/slick-util
+
+jar cmfv0 manifest windows.jar \
+    -C $src character \
+    -C $src splash \
+    -C $src event \
+    -C $root res 
+#    $lwjgl/lwjgl.jar \
+#    $lwjgl/lwjgl_util.jar \
+#    $lwjgl/jinput.jar \
+#    $slick/slick.jar \
+#    $slick_util/slick-util.jar \
