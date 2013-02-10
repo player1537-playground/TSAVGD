870e73299d3ffec9968a44f56fe692026412660b
Tanner <thobson125@gmail.com>
2013-02-10 16:09:35 -0500

    Added UpdateThread and fixed a logic bug

---
+++ Engine/src/event/UpdateThread.java
@@ -0,0 +1,21 @@
+package event;
+
+import org.lwjgl.*;
+import org.lwjgl.opengl.*;
+
+class UpdateThread extends Thread {
+    long lastTime;
+    public UpdateThread() {
+	lastTime = EventTest.getTime();
+    }
+    
+    public void run() {
+	while (EventTest.isRunning()) {
+	    System.err.println("Keyboard: " + KeyboardWrapper.toAString());
+	    long deltaTime = EventTest.getTime() - lastTime;
+	    EventTest.update((int)deltaTime);
+	    lastTime = EventTest.getTime();
+	    try { Thread.sleep(1000 / 30); } catch (Exception e) { }
+	}
+    }
+}
