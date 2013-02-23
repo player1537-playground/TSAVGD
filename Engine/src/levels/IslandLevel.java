a2552e856c412a9470209324517acdcd3c5298ec
Andrew Wintenberg <awintenb@gmail.com>
2013-02-22 19:49:35 -0500

    Added level for resources.

--- Engine/src/levels/IslandLevel.java
+++ Engine/src/levels/IslandLevel.java
@@ -1,5 +1,23 @@
 package levels;
+
 import event.*;
+import org.lwjgl.util.vector.Vector3f;
+
 public class IslandLevel extends Level {
-public void init() { EventTest.init("village_disp_fixed.obj","village_col.obj"); }
+
+    public void init() {
+        EventTest.init();
+        TerrainModel terDisp = new TerrainModel("islandD", "village_disp_fixed.obj");
+        Model terCol = new Model("islandP", "village_col.obj");
+        Terrain t = new Terrain(terDisp, terCol, EventTest.p);
+        character.Person p = new character.Person();
+        p.b.setPosition(new Vector3f(20, 20, 20));
+        addResource(terDisp);
+        addResource(terCol);
+        addResource(t);
+        EventTest.addEntity(t);
+        EventTest.addDisplayableEntity(t);
+        EventTest.addAbstractEntity(p);
+        EventTest.setTerrain(t);
+    }
 }
--- Engine/src/levels/Model.java
+++
@@ -1,7 +0,0 @@
-package conversation;
-
-public class Model {
-  public Model(String fname) {
-    System.out.println("Loaded " + fname);
-  }
-}
